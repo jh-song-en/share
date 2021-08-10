0cc17265c3e6b3a100b387f008f4d73920c2b9e8
Jaeho <jaeho.song.en@gmail.com>
2021-08-10 15:39:41 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import pyqtSlot
+from sub_form import sub_form
+
+class main_form(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        # Setting the position and size of the UI
+        self.ui = uic.loadUi(os.path.abspath('sample.ui'), self)
+        self.ui.show()
+        # Search tab
+    @pyqtSlot()
+    def browse(self):
+        sub_form()
+
+
+    def closeEvent(self, event):
+        print("closed")
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    main_form()
+
+    sys.exit(app.exec())
---
+++ sub_form.py
@@ -0,0 +1,30 @@
+import os
+from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5 import QtWebEngineWidgets
+from PyQt5.QtCore import pyqtSlot
+import pandas as pd
+import plotly.express as px
+import plotly
+
+
+class sub_form(QtWidgets.QMainWindow):
+    def __init__(self):
+        # UI_setting
+        QtWidgets.QMainWindow.__init__(self, None)
+        self.ui = uic.loadUi(os.path.abspath('plot.ui'), self)
+        self.ui.show()
+        self.a = 0
+    @pyqtSlot()
+    def plot(self):
+        self.a += 1
+        df = pd.DataFrame([[self.a+1,2],[self.a * self.a,4]], columns=["x","y"])
+        fig = px.line(df, x="x", y="x")
+        fig.update_layout(
+            template='plotly_white'
+        )
+        config = {'doubleClick': 'reset', 'displaylogo': False, 'scrollZoom': True}
+        html = '<html><head><meta charset="utf-8"/><script src="https://cdn.plot.ly/plotly-latest.min.js"></script></head><body>'
+        html += plotly.offline.plot(fig, output_type='div', include_plotlyjs='cdn', config=config)
+        html += '</body></html>'
+        self.widget.setHtml(html)
+
